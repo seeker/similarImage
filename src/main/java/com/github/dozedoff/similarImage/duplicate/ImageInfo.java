e44e636540029eea383185cc96c0ef8633ffeb5f
dozedoff <dozedoffagain@gmail.com>
2013-05-03 20:12:24 +0200

    Created class to load image info

---
+++ src/main/java/com/github/dozedoff/similarImage/duplicate/ImageInfo.java
@@ -0,0 +1,68 @@
+/*  Copyright (C) 2013  Nicholas Wright
+    
+    This file is part of similarImage - A similar image finder using pHash
+    
+    mmut is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.github.dozedoff.similarImage.duplicate;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImageInfo {
+	private final static Logger logger = LoggerFactory.getLogger(ImageInfo.class);
+	private Path path;
+	private Dimension dimension = new Dimension();
+	private long size = -1;
+	
+	public ImageInfo(Path path) {
+		this.path = path;
+		getImageData();
+	}
+	
+	private void getImageData() {
+		InputStream is;
+		try {
+			is = new BufferedInputStream(Files.newInputStream(path));
+			BufferedImage img = ImageIO.read(is);
+			
+			dimension.setSize(img.getWidth(), img.getHeight());
+			size = Files.size(path);
+		} catch (IOException e) {
+			logger.warn("Unable to get info for file {} - {}", path, e.getMessage());
+		}
+	}
+
+	public Path getPath() {
+		return path;
+	}
+
+	public Dimension getDimension() {
+		return dimension;
+	}
+
+	public long getSize() {
+		return size;
+	}
+}
