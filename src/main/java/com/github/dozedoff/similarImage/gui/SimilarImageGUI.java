1baf55fd7c114763aa5b4ebf3bf9497d16c235aa
dozedoff <dozedoffagain@gmail.com>
2013-09-09 17:07:55 +0200

    Moved logger

--- src/main/java/com/github/dozedoff/similarImage/gui/SimilarImageGUI.java
+++ src/main/java/com/github/dozedoff/similarImage/gui/SimilarImageGUI.java
@@ -54,7 +54,6 @@ import com.github.dozedoff.similarImage.duplicate.DuplicateOperations;
 
 public class SimilarImageGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private static final Logger logger = LoggerFactory.getLogger(SimilarImageGUI.class);
 
 	private final SimilarImage parent;
 	private final Persistence persistence;
@@ -258,15 +257,17 @@ public class SimilarImageGUI extends JFrame {
 	}
 
 	class GroupListPopulator implements Runnable {
+		Logger logger = LoggerFactory.getLogger(GroupListPopulator.class);
 		private List<Long> groups;
 
 		public GroupListPopulator(List<Long> groups) {
+			this.logger.info("GroupList populator created");
 			this.groups = groups;
 		}
 
 		@Override
 		public void run() {
-			logger.info("Populating group list with {} groups", groups.size());
+			this.logger.info("Populating group list with {} groups", groups.size());
 
 			groupListModel.clear();
 
@@ -277,7 +278,7 @@ public class SimilarImageGUI extends JFrame {
 				listSizeCounter++;
 
 				if (listSizeCounter >= MAX_LIST_SIZE) {
-					logger.info("Reached list size limit, aborting...");
+					this.logger.info("Reached list size limit, aborting...");
 					break;
 				}
 			}
