76b088047e617e5ce32782fde32c1eb35e2db264
dozedoff <dozedoffagain@gmail.com>
2014-01-29 23:16:14 +0100

    Added tests for filter sort

--- src/test/java/com/github/dozedoff/similarImage/duplicate/SortSimilarTest.java
+++ src/test/java/com/github/dozedoff/similarImage/duplicate/SortSimilarTest.java
@@ -17,17 +17,22 @@
  */
 package com.github.dozedoff.similarImage.duplicate;
 
-import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.dozedoff.similarImage.db.FilterRecord;
 import com.github.dozedoff.similarImage.db.ImageRecord;
 import com.github.dozedoff.similarImage.db.Persistence;
 
@@ -52,6 +57,16 @@ public class SortSimilarTest {
 		testRecords.add(new ImageRecord("/foo/foo/1", 3));
 		testRecords.add(new ImageRecord("/foo/foo/5", 5));
 		testRecords.add(new ImageRecord("/foo/foo/8", 2));
+	}
+
+	private List<FilterRecord> createFilterRecords(String reason, long... pHash) {
+		LinkedList<FilterRecord> filterRecords = new LinkedList<>();
+
+		for (long h : pHash) {
+			filterRecords.add(new FilterRecord(h, reason));
+		}
+
+		return filterRecords;
 	}
 
 	@Test
@@ -94,5 +109,119 @@ public class SortSimilarTest {
 		assertThat(sort.isEmpty(), is(false));
 		sort.clear();
 		assertThat(sort.isEmpty(), is(true));
+	}
+
+	@Test
+	public void testSortFilterHashOneGroupSize() {
+		sort.sortFilter(1, "foo", testRecords, createFilterRecords("foo", 1L));
+		List<Long> groups = sort.getDuplicateGroups();
+
+		assertThat(groups, hasItem(1L));
+		assertThat(groups.size(), is(1));
+	}
+
+	@Test
+	public void testSortFilterHashOneRecords() {
+		sort.sortFilter(1, "foo", testRecords, createFilterRecords("foo", 1L));
+
+		assertGroupOneCorrect();
+	}
+
+	@Test
+	public void testSortFilterHashTwoGroupSize() {
+		sort.sortFilter(1, "foo", testRecords, createFilterRecords("foo", 2L));
+		List<Long> groups = sort.getDuplicateGroups();
+
+		assertThat(groups, hasItem(2L));
+		assertThat(groups.size(), is(1));
+	}
+
+	@Test
+	public void testSortFilterHashTwoRecords() {
+		sort.sortFilter(1, "foo", testRecords, createFilterRecords("foo", 2L));
+
+		assertGroupTwoCorrect();
+	}
+
+	@Test
+	public void testSortFilterMultipleFiltersGroupSize() {
+		sort.sortFilter(1, "foo", testRecords, createFilterRecords("foo", 1L, 2L));
+		List<Long> groups = sort.getDuplicateGroups();
+
+		assertThat(groups, hasItem(1L));
+		assertThat(groups, hasItem(2L));
+		assertThat(groups.size(), is(2));
+	}
+
+	@Test
+	public void testSortFilterMultipleFiltersRecords() {
+		sort.sortFilter(1, "foo", testRecords, createFilterRecords("foo", 1L, 2L));
+
+		assertGroupOneCorrect();
+		assertGroupTwoCorrect();
+	}
+
+	@Test
+	public void testSortFilterHashZeroDistanceGroupSize() throws SQLException {
+		List<FilterRecord> filterRecords = createFilterRecords("foo", 4L);
+		when(mockPersistence.getAllFilters("foo")).thenReturn(filterRecords);
+
+		sort.sortFilter(0, "foo", testRecords, filterRecords);
+		List<Long> groups = sort.getDuplicateGroups();
+
+		assertThat(groups, hasItem(4L));
+		assertThat(groups.size(), is(1));
+	}
+
+	@Test
+	public void testSortFilterHashZeroDistanceRecords() throws SQLException {
+		List<FilterRecord> filterRecords = createFilterRecords("foo", 4L);
+		when(mockPersistence.getAllFilters("foo")).thenReturn(filterRecords);
+
+		sort.sortFilter(0, "foo", testRecords, filterRecords);
+
+		Set<ImageRecord> group = sort.getGroup(4L);
+
+		assertThat(group, hasItem(testRecords.get(1)));
+		assertThat(group.size(), is(1));
+	}
+
+	@Test
+	public void testSortFilterHashZeroDistanceSQLError() throws SQLException {
+		List<FilterRecord> filterRecords = createFilterRecords("foo", 4L);
+		when(mockPersistence.getAllFilters("foo")).thenThrow(new SQLException("This is a test"));
+
+		sort.sortFilter(0, "foo", testRecords, filterRecords);
+		List<Long> groups = sort.getDuplicateGroups();
+
+		assertThat(groups.size(), is(0));
+	}
+
+	@Test
+	public void testSortFilterHashZeroDistanceNoMatch() throws SQLException {
+		List<FilterRecord> filterRecords = createFilterRecords("foo", 42L);
+		when(mockPersistence.getAllFilters("foo")).thenReturn(filterRecords);
+
+		sort.sortFilter(0, "foo", testRecords, filterRecords);
+		List<Long> groups = sort.getDuplicateGroups();
+
+		assertThat(groups.size(), is(0));
+	}
+
+	private void assertGroupOneCorrect() {
+		Set<ImageRecord> group = sort.getGroup(1L);
+		assertThat(group, hasItem(testRecords.get(0)));
+		assertThat(group, hasItem(testRecords.get(2)));
+		assertThat(group, hasItem(testRecords.get(3)));
+		assertThat(group, hasItem(testRecords.get(4)));
+		assertThat(group.size(), is(4));
+	}
+
+	private void assertGroupTwoCorrect() {
+		Set<ImageRecord> group = sort.getGroup(2L);
+		assertThat(group, hasItem(testRecords.get(0)));
+		assertThat(group, hasItem(testRecords.get(3)));
+		assertThat(group, hasItem(testRecords.get(5)));
+		assertThat(group.size(), is(3));
 	}
 }
