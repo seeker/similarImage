413453f92289538750783d1d66553bf33c228a5a
dozedoff <dozedoffagain@gmail.com>
2014-01-29 20:15:16 +0100

    Singleton getInstance is now static

--- src/main/java/com/github/dozedoff/similarImage/app/Settings.java
+++ src/main/java/com/github/dozedoff/similarImage/app/Settings.java
@@ -14,7 +14,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package com.github.dozedoff.similarImage.app;
 
 import com.github.dozedoff.commonj.settings.AbstractSettings;
@@ -23,52 +23,54 @@ import com.github.dozedoff.commonj.string.Convert;
 
 public class Settings extends AbstractSettings {
 	private static Settings instance = null;
-	
-	public enum Parameters {phash_workers, data_loaders, data_loader_priority, thumbnail_dimension, loader_out_queue_size}
-	
+
+	public enum Parameters {
+		phash_workers, data_loaders, data_loader_priority, thumbnail_dimension, loader_out_queue_size
+	}
+
 	private final int DEFAULT_PHASH_WORKERS = 2;
 	private final int DEFAULT_DATA_LOADERS = 1;
 	private final int DATA_LOADER_PRIORITY = 2;
-	
+
 	private final int THUMBNAIL_DIMENSION = 500;
 	private final int LOADER_OUT_QUEUE_SIZE = 400;
-	
+
 	private Settings() {
-		super (new SettingsValidator());
+		super(new SettingsValidator());
 	}
-	
-	public synchronized Settings getInstance() {
-		if(instance == null){
+
+	public synchronized static Settings getInstance() {
+		if (instance == null) {
 			instance = new Settings();
 		}
-		
+
 		return instance;
 	}
-	
+
 	public Settings(ISettingsValidator validator) {
 		super(validator);
 	}
-	
+
 	public int getpHashWorkers() {
 		return readAndConvertProperty(Parameters.phash_workers, DEFAULT_PHASH_WORKERS);
 	}
-	
+
 	public int getDataLoaders() {
 		return readAndConvertProperty(Parameters.data_loaders, DEFAULT_DATA_LOADERS);
 	}
-	
+
 	public int getDataLoaderPriority() {
 		return readAndConvertProperty(Parameters.data_loader_priority, DATA_LOADER_PRIORITY);
 	}
-	
+
 	public int getThumbnailDimension() {
 		return readAndConvertProperty(Parameters.thumbnail_dimension, THUMBNAIL_DIMENSION);
 	}
-	
+
 	public int getLoaderOutQueueSize() {
 		return readAndConvertProperty(Parameters.loader_out_queue_size, LOADER_OUT_QUEUE_SIZE);
 	}
-	
+
 	private int readAndConvertProperty(Parameters parameter, int defaultValue) {
 		String value = properties.getProperty(parameter.toString());
 		int intValue = Convert.stringToInt(value, defaultValue);
