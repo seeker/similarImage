4537bf6da26f2561f527cdccb5828117013fa8cf
Nicholas Wright <dozedoffagain@gmail.com>
2016-08-07 16:40:13 +0200

    Read and write ExtendedAttributes

--- src/test/java/com/github/dozedoff/similarImage/IoTests.java
+++ src/test/java/com/github/dozedoff/similarImage/IoTests.java
@@ -21,11 +21,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.github.dozedoff.similarImage.io.ExtendedAttributeTest;
 import com.github.dozedoff.similarImage.io.StatisticsTest;
 
 //@formatter:off
 @RunWith(Suite.class)
 @SuiteClasses({
-		StatisticsTest.class
+		StatisticsTest.class,
+		ExtendedAttributeTest.class
 })
 public class IoTests {}
---
+++ src/test/java/com/github/dozedoff/similarImage/io/ExtendedAttributeTest.java
@@ -0,0 +1,75 @@
+/*  Copyright (C) 2016  Nicholas Wright
+    
+    This file is part of similarImage - A similar image finder using pHash
+    
+    similarImage is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.dozedoff.similarImage.io;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExtendedAttributeTest {
+	private static final String TEMP_FILE_PREFIX = "ExtendedAttributeTest";
+	private static final String TEST_NAME = "user.similarimage.junit";
+	private static final String TEST_VALUE = "foobar";
+
+	private Path tempFile;
+
+	@Before
+	public void setUp() throws Exception {
+		tempFile = Files.createTempFile(TEMP_FILE_PREFIX, null);
+	}
+
+	@Test
+	public void testSupportsExtendedAttributes() throws Exception {
+		assertThat(ExtendedAttribute.supportsExtendedAttributes(tempFile), is(true));
+	}
+
+	@Test
+	public void testSupportsExtendedAttributesNoFile() throws Exception {
+		assertThat(ExtendedAttribute.supportsExtendedAttributes(Paths.get("foo")), is(false));
+	}
+
+	@Test
+	public void testSetExtendedAttributePathStringString() throws Exception {
+		ExtendedAttribute.setExtendedAttribute(tempFile, TEST_NAME, TEST_VALUE);
+	}
+
+	@Test
+	public void testReadExtendedAttributeAsString() throws Exception {
+		ExtendedAttribute.setExtendedAttribute(tempFile, TEST_NAME, TEST_VALUE);
+
+		assertThat(ExtendedAttribute.readExtendedAttributeAsString(tempFile, TEST_NAME), is(TEST_VALUE));
+	}
+
+	@Test
+	public void testExtendedAttributeIsNotSet() throws Exception {
+		assertThat(ExtendedAttribute.isExtendedAttributeSet(tempFile, TEST_NAME), is(false));
+	}
+
+	@Test
+	public void testExtendedAttributeIsSet() throws Exception {
+		ExtendedAttribute.setExtendedAttribute(tempFile, TEST_NAME, TEST_VALUE);
+
+		assertThat(ExtendedAttribute.isExtendedAttributeSet(tempFile, TEST_NAME), is(true));
+	}
+}
