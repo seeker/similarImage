24a412ef5ec4274b34cc6e904e4a9f9128036a67
Nicholas Wright <dozedoffagain@gmail.com>
2016-08-09 11:16:49 +0200

    Moved learning test to own package

--- src/test/java/com/github/dozedoff/similarImage/learning/BKTreeLearning.java
+++ src/test/java/com/github/dozedoff/similarImage/learning/BKTreeLearning.java
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.dozedoff.similarImage;
+package com.github.dozedoff.similarImage.learning;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
