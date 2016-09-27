99b24a34a6375aa2abb685b49db70dd98125ce0e
Nicholas Wright <dozedoffagain@gmail.com>
2016-09-27 13:51:13 +0200

    Interface for Filter Repository

---
+++ core/src/main/java/com/github/dozedoff/similarImage/db/repository/FilterRepository.java
@@ -0,0 +1,71 @@
+/*  Copyright (C) 2016  Nicholas Wright
+    
+    This file is part of similarImage - A similar image finder using pHash
+    
+    similarImage is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.dozedoff.similarImage.db.repository;
+
+import java.util.List;
+
+import com.github.dozedoff.similarImage.db.FilterRecord;
+import com.github.dozedoff.similarImage.db.Thumbnail;
+
+/**
+ * Repository for retrieving and storing {@link FilterRecord} and the associated {@link Thumbnail} (if any).
+ * 
+ * @author Nicholas Wright
+ */
+public interface FilterRepository {
+	/**
+	 * Get a list of {@link FilterRecord} that match the given hash.
+	 * 
+	 * @param hash
+	 *            to search for
+	 * @return a list of found filters
+	 * @throws RepositoryException
+	 *             if the repository encounters an error processing the request
+	 */
+	List<FilterRecord> getFiltersByHash(long hash) throws RepositoryException;
+
+	/**
+	 * Get a list of {@link FilterRecord} that match the given tag.
+	 * 
+	 * @param tag
+	 *            to search for
+	 * @return a list of found filters
+	 * @throws RepositoryException
+	 *             if the repository encounters an error processing the request
+	 */
+	List<FilterRecord> getFiltersByTag(String tag) throws RepositoryException;
+
+	/**
+	 * Get a list of all {@link FilterRecord}
+	 * 
+	 * @return a list of all filters
+	 * @throws RepositoryException
+	 *             if the repository encounters an error processing the request
+	 */
+	List<FilterRecord> getAllFilters() throws RepositoryException;
+
+	/**
+	 * Persist a {@link FilterRecord}.
+	 * 
+	 * @param toStore
+	 *            the {@link FilterRecord} to persist
+	 * @throws RepositoryException
+	 *             if the repository encounters an error processing the request
+	 */
+	void storeFilter(FilterRecord toStore) throws RepositoryException;
+}
