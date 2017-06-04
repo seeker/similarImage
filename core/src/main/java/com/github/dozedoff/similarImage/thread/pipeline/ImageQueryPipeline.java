5c078bfe4bab151e3c5566e28267baa8a322669c
Nicholas Wright <dozedoffagain@gmail.com>
2017-06-04 17:46:53 +0200

    Pipeline for image queries

---
+++ core/src/main/java/com/github/dozedoff/similarImage/thread/pipeline/ImageQueryPipeline.java
@@ -0,0 +1,81 @@
+/*  Copyright (C) 2017  Nicholas Wright
+    
+    This file is part of similarImage - A similar image finder using pHash
+    
+    similarImage is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.dozedoff.similarImage.thread.pipeline;
+
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
+import com.github.dozedoff.similarImage.db.ImageRecord;
+import com.google.common.collect.Multimap;
+
+/**
+ * Pipeline for modular image queries.
+ * 
+ * @author Nicholas Wright
+ *
+ */
+public class ImageQueryPipeline implements Function<Path, Multimap<Long, ImageRecord>> {
+	private final Function<Path, List<ImageRecord>> imageQueryStage;
+	private final Function<Collection<ImageRecord>, Multimap<Long, ImageRecord>> imageGrouper;
+	private Collection<Function<Multimap<Long, ImageRecord>, Multimap<Long, ImageRecord>>> postProcessingStages;
+	
+	/**
+	 * Create a new pipeline that uses the given stages for processing queries.
+	 * 
+	 * @param imageQueryStage
+	 *            how images will be queried from a datasource
+	 * @param imageGrouper
+	 *            how images will be grouped
+	 * @param postProcessingStages
+	 *            stages for performing post-processing
+	 */
+	public ImageQueryPipeline(Function<Path, List<ImageRecord>> imageQueryStage,
+			Function<Collection<ImageRecord>, Multimap<Long, ImageRecord>> imageGrouper,
+			Collection<Function<Multimap<Long, ImageRecord>, Multimap<Long, ImageRecord>>> postProcessingStages) {
+		this.imageQueryStage = imageQueryStage;
+		this.imageGrouper = imageGrouper;
+		this.postProcessingStages = postProcessingStages;
+	}
+
+	/**
+	 * Query images with the path and apply all pipeline stages.
+	 * 
+	 * @param path
+	 *            to limit the images by scope, if null, all images will be used
+	 * 
+	 * @return images grouped by hash
+	 */
+	@Override
+	public Multimap<Long, ImageRecord> apply(Path path) {
+		List<ImageRecord> images = imageQueryStage.apply(path);
+		Multimap<Long, ImageRecord> groups = imageGrouper.apply(images);
+		return postProcessing(groups);
+	}
+
+	private Multimap<Long, ImageRecord> postProcessing(Multimap<Long, ImageRecord> groups) {
+		Multimap<Long, ImageRecord> step = groups;
+
+		for (Function<Multimap<Long, ImageRecord>, Multimap<Long, ImageRecord>> ppStage : postProcessingStages) {
+			step = ppStage.apply(step);
+		}
+
+		return step;
+	}
+}
---
+++ core/src/test/java/com/github/dozedoff/similarImage/thread/pipeline/ImageQueryPipelineTest.java
@@ -0,0 +1,99 @@
+/*  Copyright (C) 2017  Nicholas Wright
+    
+    This file is part of similarImage - A similar image finder using pHash
+    
+    similarImage is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.dozedoff.similarImage.thread.pipeline;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.github.dozedoff.similarImage.db.ImageRecord;
+import com.google.common.collect.Multimap;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ImageQueryPipelineTest {
+	@Mock
+	private Function<Path, List<ImageRecord>> imageQueryStage;
+
+	@Mock
+	private Function<Collection<ImageRecord>, Multimap<Long, ImageRecord>> grouper;
+
+	@Mock
+	private Function<Multimap<Long, ImageRecord>, Multimap<Long, ImageRecord>> postProcessingStageA;
+	@Mock
+	private Function<Multimap<Long, ImageRecord>, Multimap<Long, ImageRecord>> postProcessingStageB;
+
+	@Mock
+	private List<ImageRecord> images;
+
+	@Mock
+	private Multimap<Long, ImageRecord> groups;
+
+	private ImageQueryPipeline cut;
+
+	@Before
+	public void setUp() throws Exception {
+		when(imageQueryStage.apply(any())).thenReturn(images);
+		when(grouper.apply(images)).thenReturn(groups);
+		when(postProcessingStageA.apply(groups)).thenReturn(groups);
+		when(postProcessingStageB.apply(groups)).thenReturn(groups);
+
+		cut = new ImageQueryPipeline(imageQueryStage, grouper,
+				Arrays.asList(postProcessingStageA, postProcessingStageB));
+
+		cut.apply(null);
+	}
+
+	@Test
+	public void testQueryExecuted() throws Exception {
+		verify(imageQueryStage).apply(any());
+	}
+
+	@Test
+	public void testGroupingExecuted() throws Exception {
+		verify(grouper).apply(images);
+	}
+	
+	@Test
+	public void testPostprocessingAExecuted() throws Exception {
+		verify(postProcessingStageA).apply(groups);
+	}
+
+	@Test
+	public void testPostprocessingBExecuted() throws Exception {
+		verify(postProcessingStageB).apply(groups);
+	}
+
+	@Test
+	public void testPipelineReturnsGroups() throws Exception {
+		assertThat(cut.apply(null), is(groups));
+	}
+}
