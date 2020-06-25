43b32eb8a3d61eb8debfb9b8c092362bf22195b6
Nicholas Wright <dozedoffagain@gmail.com>
2020-06-25 20:23:26 +0200

    Updated HasherNodeTest to use Artemis for testing

--- distributed/src/test/java/com/github/dozedoff/similarImage/messaging/HasherNodeTest.java
+++ distributed/src/test/java/com/github/dozedoff/similarImage/messaging/HasherNodeTest.java
@@ -17,36 +17,40 @@
  */
 package com.github.dozedoff.similarImage.messaging;
 
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.startsWith;
-import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.nullable;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.IIOException;
 
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.api.core.client.ClientConsumer;
+import org.apache.activemq.artemis.api.core.client.ClientMessage;
+import org.apache.activemq.artemis.api.core.client.ClientProducer;
+import org.apache.activemq.artemis.api.core.client.MessageHandler;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.codahale.metrics.MetricRegistry;
 import com.github.dozedoff.commonj.hash.ImagePHash;
 
-//FIXME Silent runner is just a band-aid to get the tests to run
-@RunWith(MockitoJUnitRunner.Silent.class)
+@RunWith(MockitoJUnitRunner.class)
 public class HasherNodeTest extends MessagingBaseTest {
 	private static final String TEST_ADDRESS_REQUEST = "test_request";
 	private static final String TEST_ADDRESS_RESULT = "test_result";
@@ -62,50 +66,77 @@ public class HasherNodeTest extends MessagingBaseTest {
 	private HasherNode cut;
 	private MessageFactory messageFactory;
 	private MetricRegistry metrics;
+	private ClientMessage hashRequestMessage;
+	private ClientConsumer consumer;
+	private ClientProducer producer;
 
+	private List<ClientMessage> messages;
+	
 	@Before
 	public void setUp() throws Exception {
 		when(hasher.getLongHash(nullable(BufferedImage.class))).thenReturn(TEST_HASH);
 		messageFactory = new MessageFactory(session);
-		message = messageFactory.hashRequestMessage(TEST_DATA, TEST_UUID);
+		hashRequestMessage = messageFactory.hashRequestMessage(TEST_DATA, TEST_UUID);
 		metrics = new MetricRegistry();
 
 		cut = new HasherNode(session, hasher, TEST_ADDRESS_REQUEST, TEST_ADDRESS_RESULT, metrics);
+		consumer = session.createConsumer(TEST_ADDRESS_RESULT);
+		producer = session.createProducer(TEST_ADDRESS_REQUEST);
+		
+		messages = new LinkedList<>();
+		
+		consumer.setMessageHandler(new MessageHandler() {
+			@Override
+			public void onMessage(ClientMessage message) {
+				messages.add(message);
+			}
+		});
 	}
 
+	private ClientMessage waitForHashResult() throws ActiveMQException {
+		producer.send(hashRequestMessage);
+		
+		await().until(messages::size, is(1));
+		
+		return messages.get(0);
+	}
+	
 	@Test
-	public void testMessageSent() throws Exception {
-		cut.onMessage(message);
-
-		verify(producer).send(sessionMessage);
+	public void testMessageSentAndResponseReceived() throws Exception {
+		producer.send(hashRequestMessage);
+		
+		await().until(messages::size, is(1));
 	}
 
 	@Test
 	public void testMessageUuid() throws Exception {
-		cut.onMessage(message);
+		ClientMessage result = waitForHashResult();
+		
+		long mostSignificant = result.getBodyBuffer().readLong();
+		long leastSignificant = result.getBodyBuffer().readLong();
 
-		assertThat(sessionMessage.getBodyBuffer().readLong(), is(TEST_UUID.getMostSignificantBits()));
-		assertThat(sessionMessage.getBodyBuffer().readLong(), is(TEST_UUID.getLeastSignificantBits()));
+		assertThat(mostSignificant, is(TEST_UUID.getMostSignificantBits()));
+		assertThat(leastSignificant, is(TEST_UUID.getLeastSignificantBits()));
 	}
 
 	@Test
 	public void testMessageHash() throws Exception {
-		cut.onMessage(message);
+		ClientMessage result = waitForHashResult();
 
-		sessionMessage.getBodyBuffer().readLong();
-		sessionMessage.getBodyBuffer().readLong();
+		result.getBodyBuffer().readLong();
+		result.getBodyBuffer().readLong();
 
-		assertThat(sessionMessage.getBodyBuffer().readLong(), is(TEST_HASH));
+		assertThat(result.getBodyBuffer().readLong(), is(TEST_HASH));
 	}
 
 	@Test
 	public void testMessageCorruptImageSent() throws Exception {
-		message = new MockMessageBuilder().configureCorruptImageMessage().build();
-		when(hasher.getLongHash(any(InputStream.class))).thenThrow(new IIOException(""));
-
-		cut.onMessage(message);
-
-		verify(producer).send(sessionMessage);
+		Mockito.reset(hasher);
+		when(hasher.getLongHash(nullable(BufferedImage.class))).thenThrow(new IIOException(""));
+		
+		producer.send(hashRequestMessage);
+		
+		await().pollDelay(1, TimeUnit.SECONDS).until(messages::size, is(not(1)));
 	}
 	
 	@Test
@@ -114,8 +145,10 @@ public class HasherNodeTest extends MessagingBaseTest {
 
 		for (long i = 0; i < WORKER_NUMBER; i++) {
 			nodes.add(new HasherNode(session, hasher, TEST_ADDRESS_REQUEST, TEST_ADDRESS_RESULT, metrics));
-			cut.onMessage(message);
+			producer.send(hashRequestMessage);
 		}
+		
+		await().until(messages::size, is((int)WORKER_NUMBER));
 
 		assertThat(metrics.getMeters().get(HasherNode.METRIC_NAME_HASH_MESSAGES)
 				.getCount(), is(WORKER_NUMBER));
@@ -123,25 +156,26 @@ public class HasherNodeTest extends MessagingBaseTest {
 
 	@Test
 	public void testBufferResize() throws Exception {
-		message = messageFactory.hashRequestMessage(new byte[LARGE_DATA_SIZE], TEST_UUID);
+		hashRequestMessage = messageFactory.hashRequestMessage(new byte[LARGE_DATA_SIZE], TEST_UUID);
 
-		cut.onMessage(message);
+		cut.onMessage(hashRequestMessage);
 
 		assertThat(metrics.getMeters().get(HasherNode.METRIC_NAME_BUFFER_RESIZE).getCount(), is(1L));
 	}
 
 	@Test
 	public void testHashDuration() throws Exception {
-		cut.onMessage(message);
+		cut.onMessage(hashRequestMessage);
 
 		assertThat(metrics.getTimers().get(HasherNode.METRIC_NAME_HASH_DURATION).getSnapshot().getMean(), is(not(0.0)));
 	}
 
 	@Test
 	public void testHashDurationOnFailure() throws Exception {
+		Mockito.reset(hasher);
 		when(hasher.getLongHash(nullable(BufferedImage.class))).thenThrow(new IOException("Testing"));
 
-		cut.onMessage(message);
+		cut.onMessage(hashRequestMessage);
 
 		assertThat(metrics.getTimers().get(HasherNode.METRIC_NAME_HASH_DURATION).getSnapshot().getMean(), is(0.0));
 	}
@@ -160,7 +194,6 @@ public class HasherNodeTest extends MessagingBaseTest {
 	public void testStop() throws Exception {
 		cut.stop();
 
-		verify(consumer).close();
-		verify(producer).close();
+		assertThat(session.isClosed(), is(true));
 	}
 }
